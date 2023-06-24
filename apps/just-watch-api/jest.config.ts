e9f8566923f78d66d866d12311f4e6a0122ca276
tkorkotashvili <52171196+tkorkotashvili@users.noreply.github.com>
2023-06-24 22:07:20 +0400

    add project

---
+++ apps/just-watch-api/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'just-watch-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/just-watch-api'
+};
